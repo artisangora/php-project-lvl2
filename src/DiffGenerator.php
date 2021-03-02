27a8460394def2f89b50ec37a54c22a1850fbeaa
Georgiy Baranov <g.baranov@savetime.net>
2021-03-02 20:14:43 +0300

    #6 WIP

--- src/DiffGenerator.php
+++ src/DiffGenerator.php
@@ -8,6 +8,7 @@ use function Funct\Collection\sortBy;
 
 const TYPE_ADD = 'add';
 const TYPE_REMOVE = 'remove';
+const TYPE_UPDATE = 'update';
 const TYPE_EQUAL = 'equal';
 
 function genDiff(string $pathToFile1, string $pathToFile2, string $format = 'stylish'): string
@@ -28,34 +29,64 @@ function diff(array $data1, array $data2): array
     $keys = sortBy($keys, fn ($value) => $value);
 
     foreach ($keys as $key) { //todo вынести в ф-ю
-        if (!array_key_exists($key, $data1)) {
-            $diff[] = createDiffRow(TYPE_ADD, $key, handleValue($data2[$key]));
-        } elseif (!array_key_exists($key, $data2)) {
-            $diff[] = createDiffRow(TYPE_REMOVE, $key, handleValue($data1[$key]));
-        } elseif ($data1[$key] !== $data2[$key]) {
-            $diff[] = createDiffRow(TYPE_REMOVE, $key, handleValue($data1[$key]));
-            $diff[] = createDiffRow(TYPE_ADD, $key, handleValue($data2[$key]));
-        } elseif ($data1[$key] === $data2[$key]) {
-            $diff[] = createDiffRow(TYPE_EQUAL, $key, handleValue($data1[$key]));
-        }
+        $diff[] = createDiffRow($key, $data1, $data2);
     }
 
     return $diff;
 }
 
-function handleValue($value)
+function prepareValue($value)
 {
-    if (is_object($value)) {
-        $value = (array)$value;
-        return array_map(function ($key, $value) {
-            return createDiffRow(TYPE_EQUAL, $key, $value);
-        }, array_keys($value), $value);
-    }
     return $value;
 }
 
-function createDiffRow(string $type, string $key, $value): array
+function createDiffRow(string $key, $data1, $data2): array
 {
-    return ['type' => $type, 'key' => $key, 'value' => $value];
+    if (!array_key_exists($key, $data1)) {
+        return [
+            'type' => TYPE_ADD,
+            'key' => $key,
+            'valueBefore' => prepareValue(null),
+            'valueAfter' => prepareValue($data2[$key]),
+        ];
+    }
+
+    if (!array_key_exists($key, $data2)) {
+        return [
+            'type' => TYPE_REMOVE,
+            'key' => $key,
+            'valueBefore' => $data1[$key],
+            'valueAfter' => null,
+        ];
+    }
+
+    if (is_array($data1[$key]) && is_array($data2[$key])) {
+        return [
+            'type' => TYPE_EQUAL,
+            'key' => $key,
+            'valueBefore' => prepareValue($data1[$key]),
+            'valueAfter' => prepareValue($data2[$key]),
+        ];
+    }
+
+    if ($data1[$key] !== $data2[$key]) {
+        return [
+            'type' => TYPE_UPDATE,
+            'key' => $key,
+            'valueBefore' => prepareValue($data1[$key]),
+            'valueAfter' => prepareValue($data2[$key]),
+        ];
+    }
+
+    if ($data1[$key] === $data2[$key]) {
+        return [
+            'type' => TYPE_EQUAL,
+            'key' => $key,
+            'valueBefore' => prepareValue($data1[$key]),
+            'valueAfter' => prepareValue($data1[$key]),
+        ];
+    }
+
+    throw new \LogicException('Не удалось создать diff');
 }
 
